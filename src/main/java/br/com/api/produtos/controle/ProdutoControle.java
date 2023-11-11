1dab72a51268e003bdef89056898d9d0caa99b42
wenderson23 <wendersonkw1@gmail.com>
2023-11-11 19:46:59 -0300

    Rota para alterar produtos

--- src/main/java/br/com/api/produtos/controle/ProdutoControle.java
+++ src/main/java/br/com/api/produtos/controle/ProdutoControle.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,6 +35,11 @@ public class ProdutoControle {
     @PostMapping("/cadastrar")
     public ResponseEntity<?> casdastrar(@RequestBody ProdutoModelo pm){
     return ps.cadastrarAlterar(pm, "cadastrar");
+    }
+
+    @PutMapping("/alterar")
+     public ResponseEntity<?> alterar(@RequestBody ProdutoModelo pm){
+    return ps.cadastrarAlterar(pm, "alterar");
     }
 
 }
